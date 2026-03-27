5886fef5c7c0fe5c6c39634acd84cf6a62806e12
prasadmoulikc <prasadgowda908@gmail.com>
2026-03-27 12:52:32 +0530

    refactor: Move ErrorBoundary component
    Relocates the ErrorBoundary component to the `components` directory to improve code organization. Updates the import path in `src/main.tsx` accordingly.

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -9,7 +9,7 @@ interface ErrorBoundaryState {
   error: Error | null;
 }
 
-export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false, error: null };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
